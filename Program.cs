7895a21c6d3719daee7119021738142d67d47685
petrus <pshai671@gmail.com>
2023-01-26 13:51:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Object_Oriented_Program_which_display_different_types_of_vehicles_brands;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vehicle myvehicle = new Vehicle();
+        Vehicle MyAudiA4 = new AudiA4();
+        Vehicle myChevroletLacetti = new ChevroletLacetti();
+        Vehicle myBMWSUV = new BMWSUV();
+        Vehicle myMustangCalifornia = new MustangCalifornia();
+        Vehicle myMk3POLOGTI = new Mk3POLOGTI();
+
+        myvehicle.Vehiclebrand();
+        MyAudiA4.Vehiclebrand();
+        myChevroletLacetti.Vehiclebrand();
+        myBMWSUV.Vehiclebrand();
+        myMustangCalifornia.Vehiclebrand();
+        myMk3POLOGTI.Vehiclebrand();
+
+    }
+}
+
---
+++ Vehicle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Object_Oriented_Program_which_display_different_types_of_vehicles_brands
+{
+     class Vehicle
+    {
+        public virtual void Vehiclebrand()
+        {
+            Console.WriteLine("The vehicles have different brands");
+        }
+        class AudiA4 : Vehicle
+        {
+            public override void Vehiclebrand()
+            {
+                Console.WriteLine("The  audiA4 brand is : VW ");
+            }
+        }
+            class ChevroletLacetti: Vehicle
+            {
+                public override void Vehiclebrand()
+                {
+                    Console.WriteLine("The  chevrolet lacetti brand is : General Motors ");
+                }
+
+            }
+        class BMWSUV: Vehicle
+        {
+            public override void Vehiclebrand()
+            {
+                Console.WriteLine("The  bmw SUV brand is : BMW group ");
+            }
+
+        }
+        class MustangCalifornia : Vehicle
+        {
+            public override void Vehiclebrand()
+            {
+                Console.WriteLine("The  mustang california brand is : Ford Motor Co. ");
+            }
+
+        }
+        class Mk3POLOGTI: Vehicle
+        {
+            public override void Vehiclebrand()
+            {
+                Console.WriteLine("The  mk3 Polo GTI brand is : Volkswagen ");
+            }
+
+        }
+    }
+}
